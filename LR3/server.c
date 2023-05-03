6d4be6060082c570c2715423659de9320b45ea6b
Fatcatabuser <131601437+Fatcatabuser@users.noreply.github.com>
2023-05-03 19:40:54 +0300

    Update server.c

--- LR3/server.c
+++ LR3/server.c
@@ -9,6 +9,9 @@
 
 int main(int argc,char* argv)
 {
+    char rmessage[30] = {"SergeiSisoevI993"};
+    char valid[30]={"Message is valid"};
+    char not_valid[30] = {"Message is not valid"};
     int rcvid;
     int chid;
     char message[30];
@@ -23,7 +26,12 @@ int main(int argc,char* argv)
         rcvid = MsgReceive(chid, message, sizeof(message), NULL);
         printf("Polychili soobshenie, rcid %X \n", rcvid);
         printf("Soobshenie takoe : \"%s\".\n", message);
-        MsgReply(rcvid, EOK, message, sizeof(message));
+        if(!strcmp(rmessage,message)){
+            MsgReply(rcvid, EOK, valid, sizeof(valid));
+        }
+        else{
+        MsgReply(rcvid, EOK, not_valid, sizeof(not_valid));
+        }
         printf("\"%s\". \n", message);
     }
     sleep(5);
